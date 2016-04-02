4b0e5feb7d4ee55e1ad79026410aa65c380ed4e9
pgro <pgro@users.noreply.github.com>
2016-04-02 12:04:11 +0200

    initial commit

---
+++ DiscogsBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiscogsBrowser
+//
+//  Created by Peter Großmann on 02.04.16.
+//  Copyright © 2016 Peter Großmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
